bf1f84fc1b9c3520033d1c372a1e313915c4117a
Jahongir <131964623+jahong1r-t@users.noreply.github.com>
2025-02-12 17:31:13 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,15 @@
-import db.DataSource;
 import service.AuthService;
 import service.NotificationService;
 
 public class Main {
     public static void main(String[] args) {
-        while (true) {
-            DataSource.refresh();
-            try {
-                new NotificationService().service();
-                new AuthService().service();
-
-                Thread.sleep(5000);
-                System.out.println("The program is closed...");
-            } catch (Exception ignored) {
-            }
+        try {
+            NotificationService notificationService = new NotificationService();
+            notificationService.service();
+            new AuthService().service();
+            notificationService.stopService();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
